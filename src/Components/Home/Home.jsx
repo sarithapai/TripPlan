57651e9462103b8259571e1066fde0b38b383801
akshitha23 <akshithasalian8@gmail.com>
2022-06-16 14:14:26 +0530

    Components

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../Header/Header";
+import Body from "../Body/Body";
+import Footer from "../Footer/Footer";
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <Body />
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
